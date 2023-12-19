97eb2725048e460d6b2bd90c53ee4eeef156d9d1
jean.connory <jean.connory@outlook.com>
2023-12-19 15:42:09 -0300

    Lista horizontal

---
+++ ListaHorizontal/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ListaHorizontal
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class ViewModel
+	{
+        public List<string> Devices { get; set; }
+
+        public ViewModel()
+        {
+			Devices = new List<string>()
+			{
+				"TV",
+				"Ar-Condicionado",
+				"Video Game",
+				"Computador",
+				"Notebook"
+			};
+        }
+    }
+
+}
